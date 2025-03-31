1312fa85a4c24fed530a842b7bcff3c556691d87
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-04-01 00:35:46 +0100

    feat: Create logic to update image's data

--- controllers/image.js
+++ controllers/image.js
@@ -3,6 +3,18 @@ const { validationResult } = require("express-validator");
 const validate = require("../middlewares/validator");
 const prisma = new PrismaClient();
 
+async function getImages(req, res) {
+  try {
+    const images = await prisma.image.findMany();
+    await prisma.$disconnect();
+    return res.json(images);
+  } catch (e) {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+}
+
 const createImage = [
   validate.imageForm,
   async (req, res) => {
@@ -12,8 +24,8 @@ const createImage = [
     if (!result.isEmpty()) {
       return res.status(400).json({ errors: result.array() });
     }
-    const { imageName, imageUrl, itemsData } = req.body;
     try {
+      const { imageName, imageUrl, itemsData } = req.body;
       const response = await prisma.image.create({
         data: { imageName, imageUrl, itemsData },
       });
@@ -27,4 +39,28 @@ const createImage = [
   },
 ];
 
-module.exports = { createImage };
+const updateImage = [
+  validate.imageForm,
+  async (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
+    try {
+      const id = +req.params.id;
+      const { imageName, imageUrl, itemsData } = req.body;
+      const image = await prisma.image.update({
+        where: { id },
+        data: { imageName, imageUrl, itemsData },
+      });
+      await prisma.$disconnect();
+      return res.json(image);
+    } catch (e) {
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+    }
+  },
+];
+
+module.exports = { getImages, createImage, updateImage };
--- routes/image.js
+++ routes/image.js
@@ -3,6 +3,8 @@ const controller = require("../controllers/image");
 
 const router = Router();
 
+router.get("/", controller.getImages);
 router.post("/", controller.createImage);
+router.put("/:id", controller.updateImage);
 
 module.exports = router;
