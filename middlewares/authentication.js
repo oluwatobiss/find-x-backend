604523136a3b72c72f86de74339344b7036acc6e
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-05-07 16:22:05 +0100

    feat: Let only admins access the getusers controller

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,6 +1,9 @@
 const jwt = require("jsonwebtoken");
 
 function authenticateUser(req, res, next) {
+  console.log("=== authenticateUser ===");
+  console.log(req.query);
+
   const authHeader = req.headers["authorization"];
   const userToken = authHeader && authHeader.split(" ")[1];
   if (!userToken) {
@@ -10,6 +13,16 @@ function authenticateUser(req, res, next) {
     if (err) {
       return res.status(403).json({ message: "Invalid verification token" });
     }
+
+    if (req.query.status !== "ADMIN") {
+      return res
+        .status(403)
+        .json({ message: "You don't have the valid access credentials" });
+    }
+
+    console.log("===  authenticateUser ===");
+    console.log("Is Valid User");
+
     req.user = decoded;
     next();
   });
